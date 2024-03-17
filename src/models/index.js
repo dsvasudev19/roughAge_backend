e5f1f168125425ab795493cffba4984dcf81bdc3
dsvasudev <vasudevds1729@gmail.com>
2024-03-17 11:00:48 +0530

    update

--- src/models/index.js
+++ src/models/index.js
@@ -5,14 +5,14 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/database.js')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  // sequelize = new Sequelize(process.env.DATABASE_URL, config);
+  // sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
